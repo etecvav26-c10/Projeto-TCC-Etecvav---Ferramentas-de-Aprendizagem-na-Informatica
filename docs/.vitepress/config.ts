0c9c3e1e904259ecdd07c0e8d0f833e7957708c7
Grupo-Projeto-Magno <264884122+etecvav26-c10@users.noreply.github.com>
2026-06-20 16:29:55 -0300

    Update config.ts

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitepress';
 
 export default defineConfig({
   srcDir: 'docs',
-  base: '/Projeto-TCC-Etecvav---Ferramentas-de-Aprendizagem-na-Informatica/docs/',
+  base: '/Projeto-TCC-Etecvav---Ferramentas-de-Aprendizagem-na-Informatica/',
   lang: 'pt-BR',
   title: 'Infohub',
   description: 'Portal de ferramentas de aprendizagem em programação.',
