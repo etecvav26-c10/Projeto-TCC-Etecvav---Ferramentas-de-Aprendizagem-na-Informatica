26d178c1898283c9c0a1ec7c0d1e282bbfcd6310
Grupo-Projeto-Magno <264884122+etecvav26-c10@users.noreply.github.com>
2026-06-19 23:02:48 -0300

    Update Viti

---
+++ TCC 2026/Produto/Website_TCC/Viti/docs/.vitepress/config.ts
@@ -0,0 +1,53 @@
+import { defineConfig } from 'vitepress';
+
+// refer https://vitepress.dev/reference/site-config for details
+export default defineConfig({
+  lang: 'en-US',
+  title: 'VitePress',
+  description: 'Vite & Vue powered static site generator.',
+
+  themeConfig: {
+    nav: [
+      { text: 'Example', link: '/example' },
+
+      // {
+      //   text: 'Dropdown Menu',
+      //   items: [
+      //     { text: 'Item A', link: '/item-1' },
+      //     { text: 'Item B', link: '/item-2' },
+      //     { text: 'Item C', link: '/item-3' },
+      //   ],
+      // },
+
+      // ...
+    ],
+
+    sidebar: [
+      {
+        items: [
+          {
+            text: 'Guia de introdução à programação',
+            items: [
+              { text: 'Ferramentas básicas', link: '/Ferramentas_B' },
+              { text: 'Intermediárias', link: '/Guia_Intermediario' },
+              { text: 'Avançadas', link: '/Guia_Avançado' },
+            ],
+          },
+          {
+            text: 'Biblioteca de ensino à programação',
+            items: [
+              {
+                text: 'Ferramentas de Linguagem Própria',
+                items: [{ text: 'Scratch', link: '/Scratch_D' }],
+              },
+              {
+                text: 'Linguagem Python',
+                items: [{ text: 'The Farmer Was Replaced', link: '/TFWR_D' }],
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+});
