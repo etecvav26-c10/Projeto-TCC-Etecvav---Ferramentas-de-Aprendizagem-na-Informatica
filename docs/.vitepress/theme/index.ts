f20e6054531cdeaf84b86d84ccd31c0676a0d8a5
Grupo-Projeto-Magno <264884122+etecvav26-c10@users.noreply.github.com>
2026-06-20 16:46:26 -0300

    Update index.ts

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -3,7 +3,6 @@ import DefaultTheme from 'vitepress/theme'
 export default {
   extends: DefaultTheme,
   enhanceApp({ app }) {
-    // Registra o componente globalmente para o index.md encontrá-lo
-    app.component('HomePage', HomePage)
+
   }
 }
