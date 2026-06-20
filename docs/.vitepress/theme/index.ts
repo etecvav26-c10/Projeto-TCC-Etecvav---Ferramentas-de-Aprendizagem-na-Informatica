c26f0c0b8b59861e55f9170cd9c27b8f2262cc9c
Grupo-Projeto-Magno <264884122+etecvav26-c10@users.noreply.github.com>
2026-06-20 15:06:47 -0300

    Testes

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,9 +1,10 @@
-
 import DefaultTheme from 'vitepress/theme'
 import HomePage from '../../components/HomePage.vue'
+import './Hub_Principal.css'
 
 export default {
   ...DefaultTheme,
+
   enhanceApp({ app }) {
     app.component('HomePage', HomePage)
   }
