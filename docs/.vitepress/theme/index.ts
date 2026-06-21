952cb51e7a1f1349f81d4a82508f9e9ae214556e
Grupo-Projeto-Magno <264884122+etecvav26-c10@users.noreply.github.com>
2026-06-21 14:12:33 -0300

    Teste com o vue

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,9 +1,11 @@
 import DefaultTheme from 'vitepress/theme'
-import { inBrowser } from 'vitepress'
+import './style.css'
+
 import ScratchPage from './components/ScratchPage.vue'
 
 export default {
   extends: DefaultTheme,
+
   enhanceApp({ app }) {
     app.component('ScratchPage', ScratchPage)
   }
