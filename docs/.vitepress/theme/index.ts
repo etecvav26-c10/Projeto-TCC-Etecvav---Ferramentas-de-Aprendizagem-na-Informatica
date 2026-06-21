ed17ada4e2b2d5d663b77f8069288f5be228f8df
Grupo-Projeto-Magno <264884122+etecvav26-c10@users.noreply.github.com>
2026-06-21 14:03:31 -0300

    Update index.ts

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,6 +1,10 @@
 import DefaultTheme from 'vitepress/theme'
 import { inBrowser } from 'vitepress'
+import ScratchPage from './components/ScratchPage.vue'
 
 export default {
   extends: DefaultTheme,
+  enhanceApp({ app }) {
+    app.component('ScratchPage', ScratchPage)
+  }
 }
