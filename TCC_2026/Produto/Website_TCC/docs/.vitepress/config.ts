e1c29e6bd7d1ee7bd2b73c8b1698a46bbce32eb9
Grupo-Projeto-Magno <264884122+etecvav26-c10@users.noreply.github.com>
2026-09-08 17:24:18 -0300

    Update config.ts

--- TCC_2026/Produto/Website_TCC/docs/.vitepress/config.ts
+++ TCC_2026/Produto/Website_TCC/docs/.vitepress/config.ts
@@ -31,6 +31,7 @@ export default defineConfig({
                     link: '/Page_A_scratch.html',
                     target: '_self',
                   },
+                  
                 ],
               },
 
@@ -43,6 +44,13 @@ export default defineConfig({
                     link: `/Page_A_CodeCombat.html`,
                     target: '_self',
                   },
+                  {
+                    text: 'Check.io',
+                    link: `/Page_A_Checkio.html`,
+                    target: '_self',
+                  },
+                  
+                  
                 ],
               },
 
@@ -55,8 +63,94 @@ export default defineConfig({
                     link: `/Page_A_TFWR.html`,
                     target: '_self',
                   },
+                  
                 ],
               },
+              {
+                text: 'Linguagem CSS',
+
+                items: [
+                  {
+                    text: 'Minigames de CSS',
+                    link: `/Page_MinigamesCss.html`,
+                    target: '_self',
+                  },
+                  
+                ],
+              },
+              
+
+            ],
+          },
+
+          {
+            text: 'Plataformas de aprendizagem',
+
+            items: [
+              {
+                text: 'Plataformas de Cursos',
+
+                items: [
+                  {
+                    text: 'Curso em Vídeo',
+                    link: 'https://www.cursoemvideo.com/',
+                    target: '_self',
+                  },
+                  {
+                    text: 'Codeacademy',
+                    link: '/Page_A_Codeacademy.html',
+                    target: '_self',
+                  },
+                  {
+                    text: 'CodeAi',
+                    link: '/Page_A_CodeAi.html',
+                    target: '_self',
+                  },
+                  {
+                    text: 'Codechef',
+                    link: '/Page_A_Codechef.html',
+                    target: '_self',
+                  },
+                  {
+                    text: 'Exercism',
+                    link: '/Page_A_Exercism.html',
+                    target: '_self',
+                  },
+                  {
+                    text: 'Khan Academy',
+                    link: '/Page_A_KhanAcademy.html',
+                    target: '_self',
+                  },
+                ],
+              },
+              
+
+              {
+                text: 'Plataformas de Treino',
+
+                items: [
+                  {
+                    text: 'Code Combat',
+                    link: `/Page_A_CodeCombat.html`,
+                    target: '_self',
+                  },
+                  
+                ],
+              },
+
+              {
+                text: 'Plataformas de Revisão',
+
+                items: [
+                  {
+                    text: 'The Farmer Was Replaced',
+                    link: `/Page_A_TFWR.html`,
+                    target: '_self',
+                  },
+                ],
+              },
+              
+
             ],
           },
         ],
