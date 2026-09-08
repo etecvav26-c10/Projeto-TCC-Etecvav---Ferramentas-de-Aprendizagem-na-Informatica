0a8825c90e09db6952e6287693188af4073b9c78
Grupo-Projeto-Magno <264884122+etecvav26-c10@users.noreply.github.com>
2026-09-08 16:43:03 -0300

    Teste ajuste Vitepress

--- TCC_2026/Produto/Website_TCC/docs/.vitepress/config.ts
+++ TCC_2026/Produto/Website_TCC/docs/.vitepress/config.ts
@@ -29,6 +29,7 @@ export default defineConfig({
                   {
                     text: 'Scratch',
                     link: '/Page_A_Scratch',
+                    target: '_self',
                   },
                 ],
               },
