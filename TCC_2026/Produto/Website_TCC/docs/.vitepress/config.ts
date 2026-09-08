b5296b6dc20f9901506b97040ac81752a7d03b80
Grupo-Projeto-Magno <264884122+etecvav26-c10@users.noreply.github.com>
2026-09-07 21:45:10 -0300

    Update config.ts

--- TCC_2026/Produto/Website_TCC/docs/.vitepress/config.ts
+++ TCC_2026/Produto/Website_TCC/docs/.vitepress/config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vitepress';
 
+const base = '/Projeto-TCC-Etecvav---Ferramentas-de-Aprendizagem-na-Informatica/';
+
 export default defineConfig({
-  base: '/Projeto-TCC-Etecvav---Ferramentas-de-Aprendizagem-na-Informatica/',
+  base,
   lang: 'pt-BR',
   title: 'Infohub',
   description: 'Portal de ferramentas de aprendizagem em programação.',
@@ -18,20 +20,42 @@ export default defineConfig({
             text: 'Guia de introdução à programação',
             link: '/Guias.html',
           },
+
           {
             text: 'Ferramentas de aprendizagem',
+
             items: [
               {
                 text: 'Ferramentas de Linguagem Própria',
-                items: [{ text: 'Scratch', link: '/Page_A_scratch.html' }],
+
+                items: [
+                  {
+                    text: 'Scratch',
+                    link: `${base}Page_A_scratch.html`,
+                  },
+                ],
               },
+
               {
                 text: 'Ferramentas de Multiplas Linguagens',
-                items: [{ text: 'Code Combat', link: '/public/Page_A_CodeCombat.html' }],
+
+                items: [
+                  {
+                    text: 'Code Combat',
+                    link: `${base}Page_A_CodeCombat.html`,
+                  },
+                ],
               },
+
               {
                 text: 'Linguagem Python',
-                items: [{ text: 'The Farmer Was Replaced', link: '/Page_A_' }],
+
+                items: [
+                  {
+                    text: 'The Farmer Was Replaced',
+                    link: `${base}Page_A_TFWR.html`,
+                  },
+                ],
               },
             ],
           },
