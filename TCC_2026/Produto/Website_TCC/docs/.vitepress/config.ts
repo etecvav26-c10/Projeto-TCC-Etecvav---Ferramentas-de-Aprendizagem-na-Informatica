8b7d50ad05269d5e1da435d305bf128d6f31b272
Grupo-Projeto-Magno <264884122+etecvav26-c10@users.noreply.github.com>
2026-09-08 18:18:51 -0300

    Adição de elementos para a Barra de navegação

--- TCC_2026/Produto/Website_TCC/docs/.vitepress/config.ts
+++ TCC_2026/Produto/Website_TCC/docs/.vitepress/config.ts
@@ -55,31 +55,135 @@ export default defineConfig({
               },
 
               {
-                text: 'Linguagem Python',
+            text: 'Ferramentas de Linguagens Específica',
 
-                items: [
-                  {
-                    text: 'The Farmer Was Replaced',
-                    link: `/Page_A_TFWR.html`,
-                    target: '_self',
-                  },
-                  
-                ],
-              },
+            items: [
               {
-                text: 'Linguagem CSS',
-
                 items: [
                   {
-                    text: 'Minigames de CSS',
-                    link: `/Page_MinigamesCss.html`,
-                    target: '_self',
+                  text: 'Linguagem Python',
+
+                  items: [
+                    {
+                      text: 'The Farmer Was Replaced',
+                      link: `/Page_A_TFWR.html`,
+                      target: '_self',
+                    },
+                    {
+                      text: 'Learn Python',
+                      link: `https://www.learnpython.org/`,
+                      target: '_self',
+                    },
+                    {
+                      text: 'Python Institute',
+                      link: `https://pythoninstitute.org/`,
+                      target: '_self',
+                    },
+                    
+                    ],
+                  },  
+                  {
+                  text: 'Linguagem C#',
+
+                  items: [
+                    {
+                      text: 'Unity Learn',
+                      link: `https://learn.unity.com/`,
+                      target: '_self',
+                    },
+                    
+                    ],
                   },
-                  
+                  {
+                  text: 'Linguagem java',
+
+                  items: [
+                    {
+                      text: 'MOOC.fi',
+                      link: `https://www.mooc.fi/en/`,
+                      target: '_self',
+                    },
+                    
+                    ],
+                  },  
+                  {
+                  text: 'Linguagem javascript',
+
+                  items: [
+                    {
+                      text: 'MDN Web Docs',
+                      link: `https://developer.mozilla.org/en-US/docs/Web/JavaScript`,
+                      target: '_self',
+                    },
+                    {
+                      text: 'Javascript Info',
+                      link: 'https://javascript.info/',
+                      target: '_self',
+                    },
+                    {
+                      text: 'The Elevator Saga',
+                      link: 'https://play.elevatorsaga.com/',
+                      target: '_self',
+                    },
+                    {
+                      text: 'Screeps',
+                      link: 'https://store.screeps.com/pt-BR',
+                      target: '_self',
+                    },
+                    {
+                      text: 'Untrusted',
+                      link: 'https://untrustedgame.com/',
+                      target: '_self',
+                    },
+                    
+                    ],
+                  },
+                  {
+                  text: 'Linguagem PHP',
+
+                  items: [
+                    {
+                      text: 'PHP Plataforma Laracasts',
+                      link: `https://laracasts.com/series/php-for-beginners-2023-edition`,
+                      target: '_self',
+                    },
+                    
+                    ],
+                  },
+                  {
+                  text: 'Linguagem SQL',
+
+                  items: [
+                    {
+                      text: 'Sql Bolt',
+                      link: `https://sqlbolt.com/`,
+                      target: '_self',
+                    },
+                    {
+                      text: 'Sql Zoo',
+                      link: `https://sqlzoo.net/wiki/SQL_Tutorial`,
+                      target: '_self',
+                    },
+                    {
+                      text: 'Sql Murder Mystery',
+                      link: `https://mystery.knightlab.com/`,
+                      target: '_self',
+                    },
+                    {
+                      text: 'Sql Poliece Departament',
+                      link: `https://sqlpd.com/`,
+                      target: '_self',
+                    },
+                    ],
+                  },              
                 ],
+
+                
               },
               
-
+            ],
+          },
+                
             ],
           },
 
@@ -94,6 +198,11 @@ export default defineConfig({
                   {
                     text: 'Curso em Vídeo',
                     link: 'https://www.cursoemvideo.com/',
+                    target: '_self',
+                  },
+                  {
+                    text: 'Coddy Tech',
+                    link: 'https://coddy.tech/',
                     target: '_self',
                   },
                   {
@@ -121,6 +230,16 @@ export default defineConfig({
                     link: '/Page_A_KhanAcademy.html',
                     target: '_self',
                   },
+                  {
+                    text: 'Turtle Academy',
+                    link: `/Page_A_Turtleacademy.html`,
+                    target: '_self',
+                  },
+                  {
+                    text: 'Tynker',
+                    link: `/Page_A_Tynker.html`,
+                    target: '_self',
+                  },
                 ],
               },
               
@@ -130,10 +249,32 @@ export default defineConfig({
 
                 items: [
                   {
-                    text: 'Code Combat',
-                    link: `/Page_A_CodeCombat.html`,
+                    text: 'CodeWars',
+                    link: `/Page_A_Codewars.html`,
                     target: '_self',
                   },
+                  {
+                    text: 'Codingame',
+                    link: `/Page_A_Codingame.html`,
+                    target: '_self',
+                  },
+                  {
+                    text: 'Hacker Rank',
+                    link: `/Page_A_Hackerank.html`,
+                    target: '_self',
+                  },
+                  {
+                    text: 'LeetCode',
+                    link: `/Page_A_Leetcode.html`,
+                    target: '_self',
+                  },
+                  {
+                    text: 'CodeGym',
+                    link: `https://codegym.cc/`,
+                    target: '_self',
+                  },
+                  
+                  
                   
                 ],
               },
@@ -143,8 +284,8 @@ export default defineConfig({
 
                 items: [
                   {
-                    text: 'The Farmer Was Replaced',
-                    link: `/Page_A_TFWR.html`,
+                    text: 'W3Schools',
+                    link: `/Page_A_W3Schools.html`,
                     target: '_self',
                   },
                 ],
@@ -153,6 +294,8 @@ export default defineConfig({
 
             ],
           },
+
+          
         ],
       },
     ],
